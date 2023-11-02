fe18acb61d99753ec524508f2dad002a9e4148cf
Robert Crupa <robert.crupa@clearcom.com>
2023-11-02 13:08:34 +0000

    Create state variables for agent

--- src/agent.py
+++ src/agent.py
@@ -2,6 +2,10 @@ from __future__ import annotations
 
 from collections import deque
 
+import numpy as np
+from game import BLOCK_SIZE
+from game import Direction
+from game import Point
 from game import SnakeGame
 
 MAX_MEMORY = 100_000  # Maximum number of experiences we are storing
@@ -20,7 +24,54 @@ class Agent:
         pass
 
     def get_state(self, game):
-        pass
+        head = game.snake[0]
+
+        # Points around the head
+        point_l = Point(head.x - BLOCK_SIZE, head.y)
+        point_r = Point(head.x + BLOCK_SIZE, head.y)
+        point_u = Point(head.x, head.y - BLOCK_SIZE)
+        point_d = Point(head.x, head.y + BLOCK_SIZE)
+
+        # Directions
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        # Create agent state
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)),
+
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+
+            # Food location
+            game.food.x < game.head.x,  # Food left
+            game.food.x > game.head.x,  # Food right
+            game.food.y < game.head.y,  # Food up
+            game.food.y > game.head.y,  # Food down
+        ]
+
+        return np.array(state, dtype=int)
 
     def remember(Self, state, action, reward, next_state, done):
         pass
@@ -45,7 +96,7 @@ def train():
 
     game = SnakeGame()
 
-    while (True):
+    while True:
 
         # Get old state
         state_old = agent.get_state(game)
@@ -72,12 +123,15 @@ def train():
             agent.train_long_memory()
 
             record = max(score, record)
+            # TODO: save record model
 
             print(
                 'Game', agent.number_of_games,
                 'Score', score,
                 'Record:', record,
             )
+
+            # TODO: plot progress
 
 
 if __name__ == '__main__':
